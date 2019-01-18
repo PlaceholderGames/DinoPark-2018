ad26d35d4f4e7f7bf87ef7b4d4e2ba849690fa26
Monofoot <Robertofield@hotmail.co.uk>
2019-01-18 16:54:49 +0000

    Initial skeleton for a boid. Next is to fill the functions and it should hopefully be quite simple.

---
+++ Assets/Scripts/BoidScripts/AgentBoid.cs
@@ -0,0 +1,42 @@
+﻿// Perhaps my favourite name of a file ever. I think I'll call each bird that.
+// Agent Boid.
+// The purpose of this script is to be attached to an agent prefab.
+// BoidController.cs will spawn these prefabs and this script will
+// set rules and behaviour. Hopefully some fun emergent behaviour
+// appears as a result.
+// There are three primary tenants to a boid algorithm:
+// Separation // Alignment // Cohesion //
+// Hopefully these can be summarized in one function each
+// and unleashed in the Update function, free for Agent Boid
+// to go where he or she pleases.
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentBoid : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void Separation ()
+    {
+
+    }
+
+    void Alignmnent ()
+    {
+
+    }
+
+    void Cohesion()
+    {
+
+    }   
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
