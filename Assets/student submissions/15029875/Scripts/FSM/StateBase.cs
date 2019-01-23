f9809273ff4795ad6935b4cca5822b10e4094852
Field R (FCES) <15029875@glam.ac.uk>
2019-01-23 13:29:54 +0000

    Initial FSM staging. Will fill in later.

---
+++ Assets/student submissions/15029875/Scripts/FSM/StateBase.cs
@@ -0,0 +1,12 @@
+﻿// State base class.
+// All states inherit this class and methods will be overridden from here.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class StateBase {
+
+    // Get a reference to the game object.
+
+}
---
+++ Assets/student submissions/15029875/Scripts/FSM/StateMachine.cs
@@ -0,0 +1,23 @@
+﻿// Owned by an agent.
+// Manages the behaviour behaviour for said agent.
+// Every possible state availalbe to the agent
+// exists in this StateMachine.
+// The StateMachine also holds a pointer
+// for the current state.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachine : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
