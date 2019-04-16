1e8e71225b11cbfb986a47cb0f50c76d5a0e177b
Mariya <lencea@abv.bg>
2019-04-16 16:32:50 +0100

    idle state
    created a new state machine, involving the animatior that unity provides - also created an idle state for the raptor

---
+++ Assets/16019873/IdleState.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleState : StateMachineBehaviour
+{
+    //first object is to grab and get hold of the water object
+    GameObject moveDino;
+    //loop through this array to move the dino around and make him look at the surroundings
+    GameObject[] waypoints;
+    //check which waypoint we are currently at when swapping states
+    int currentWaypoint;
+
+    //find all game objects with this tag
+    void Awake()
+    {
+        waypoints = GameObject.FindGameObjectsWithTag("waypoint");
+    }
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    //this will be executed no matter what the dinos are doing at this moment
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        //even if the 'quest' that the dino is doing at the moment, it would still go back to the original position
+        //and restart 'idle' again after being interrupted
+        moveDino = animator.gameObject;
+        currentWaypoint = 0;
+
+	}
+
+	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        //this function will run over and over again until the dino is in this state
+        if (waypoints.Length == 0) return;
+        //if a waypoint is reached 
+        //then update and get a new one
+        //as it starts at 0
+        if(Vector3.Distance(waypoints[currentWaypoint].transform.position, moveDino.transform.position) < 3.0f)
+        {
+            currentWaypoint++;
+            //creates a circuit of waypoints
+            if(currentWaypoint >= waypoints.Length)
+            {
+                currentWaypoint = 0;
+            }
+        }
+
+        //rotate towards the target that has been detected
+        //move the dino forward
+        //slerp is to slowly turn and start facing the waypoint
+        var direction = waypoints[currentWaypoint].transform.position - moveDino.transform.position;
+        moveDino.transform.rotation = Quaternion.Slerp(moveDino.transform.rotation, Quaternion.LookRotation(direction), 1.0f * Time.deltaTime);
+        //pushing it forward to the z axis
+        moveDino.transform.Translate(0, 0, Time.deltaTime * 2.0f);
+
+	}
+
+	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+	
+	}
+
+	
+}
