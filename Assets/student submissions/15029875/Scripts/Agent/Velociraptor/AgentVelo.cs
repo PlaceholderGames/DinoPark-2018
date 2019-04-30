9570eeda42c38faf434a8c62111e22f88c5e055a
Monofoot <Robertofield@hotmail.co.uk>
2019-04-30 17:00:00 +0100

    Removed all trace of the Bison - very sad days :( Moved the state machine to the default anky and velociraptor prefabs, going to start actually writing their idle (herd) scripts now.

---
+++ Assets/student submissions/15029875/Scripts/Agent/Velociraptor/AgentVelo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using StateMachineInternals;
+using UnityEngine;
+
+// The Bison agent is special in that it has the capacity to herd
+// as well as idle.
+public class AgentVelo : AgentBase
+{
+    private StateMachine stateMachine = new StateMachine();
+
+    // Constructor.
+    private void Start()
+    {
+        // Extra health... bison are tough.
+        this.health = 200;
+        this.speed = 1;
+        this.stateMachine.SwitchState(new TestState());
+    }
+
+    public override void Update()
+    {
+        this.stateMachine.Update();
+    }
+}
